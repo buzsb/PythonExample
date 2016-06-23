e282ff17b1f44ddb28a21cc39b1775e96c39f02b
Buz Oleksandr <oleksandr.buz@gmail.com>
2016-06-23 11:15:14 +0300

    add file Fibonacci_test, corect Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,22 +1,28 @@
 #Finding Fibonacci number in two ways, first simple summing, second recursive method
 def fib(n):
-    if n < 1:
+    if n < 1 or type(n) == str:
         raise ValueError
     a = 1
     b = 1
     for i in range(n - 1):
         a, b = b, a + b
     return a
-try:
-    print fib(17)
-except ValueError:
-    print "hi"
+
 
 
 def rfib(n):
+    if type(n) == str:
+        return "This is not a namber"
     if n < 2:
         return n
     a = rfib(n - 1) + rfib(n - 2)
     return a 
 
-print rfib(17)
+if __name__ == '__main__':
+    try:
+        print fib('dd')
+    except ValueError:
+        print "What is wrong with you?"
+    
+    print rfib(12)
+    
---
+++ Tests/Fibonacci_test.py
@@ -0,0 +1,35 @@
+import unittest
+from Fibonacci import fib, rfib
+
+class FibonacciTest(unittest.TestCase):
+    def test_equality(self):
+        self.assertEqual(fib(6), 8)
+        self.assertEqual(fib(17), 1597)
+
+    def test_negative(self):
+        with self.assertRaises(ValueError):
+            fib(-2)
+            fib(-10)
+
+    def test_string_argument(self):
+        with self.assertRaises(ValueError):
+            fib('string')
+            fib('no more')
+            
+
+class RecursiveFibonacciTest(unittest.TestCase):
+    def test_equality(self):
+        self.assertEqual(rfib(6), 8)
+        self.assertEqual(rfib(17), 1597)
+
+    def test_negative(self):
+        self.assertEqual(rfib(-1), -1)
+        self.assertEqual(rfib(-5), -5)
+
+    def test_string_argument(self):
+        self.assertEqual(type(rfib('aa')), str)
+        self.assertEqual(rfib('more'), "This is not a namber")
+
+
+if __name__ == '__main__':
+    unittest.main()
